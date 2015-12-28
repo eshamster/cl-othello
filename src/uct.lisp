8d16a44fad2863affc0873e2a71aca8bd5d45193
eshamster <hamgoostar@gmail.com>
2015-12-29 03:38:30 +0900

    Refactor uct.lisp slightly in general

--- src/uct.lisp
+++ src/uct.lisp
@@ -40,7 +40,7 @@
 
 (defun make-a-uct-node (move-from-parent)
   (list (make-uct-node
-         :unexpanded-moves 'has-not-made-child
+         :unexpanded-moves :has-not-made-child
          :move-from-parent move-from-parent
          :num 0
          :sum 0)))
@@ -64,29 +64,29 @@
   (let* ((node (get-node-value tree))
          (moves (uct-node-unexpanded-moves node))
          (expand-intv (uct-param-expand-intv uct-param)))
-    (if (eq moves 'has-not-made-child)
-        (progn (setf moves (mapcar-move-store #'(lambda (m) (clone-move m)) (make-moves game)))
-               (setf (uct-node-unexpanded-moves node) moves)))
+    (when (eq moves :has-not-made-child)
+      (setf moves (mapcar-move-store (lambda (m) (clone-move m))
+                                     (make-moves game)))
+      (setf (uct-node-unexpanded-moves node) moves))
     (let ((move (car moves)))
-      (if (or (null move)
-              (eq (uct-node-num node) 0)
-              (not (eq (mod (uct-node-num node) expand-intv) (- expand-intv 1))))
-          (return-from expand-child-if-needed tree))
-      (setf (uct-node-unexpanded-moves node) (cdr moves))
-      (push-child tree (make-a-uct-node move)))))
+      (when (and move
+                 (> (uct-node-num node) 0)
+                 (eq (mod (uct-node-num node) expand-intv) (- expand-intv 1))) 
+        (setf (uct-node-unexpanded-moves node) (cdr moves))
+        (push-child tree (make-a-uct-node move)))))
+  tree)
     
 (defun select-uct-child (game parent uct-param)
-  (if (not (has-children parent))
-      (return-from select-uct-child nil))
-  (let ((ucb-coef (uct-param-ucb-coef uct-param))
-        (now-turn (game-turn game)))
-    (select-max-child (lambda (node)
-                        (calc-ucb (uct-node-sum node)
-                                  (uct-node-num node)
-                                  (uct-node-num (get-node-value parent))
-                                  :coef  ucb-coef
-                                  :turn now-turn))
-                      parent)))
+  (when (has-children parent) 
+    (let ((ucb-coef (uct-param-ucb-coef uct-param))
+          (now-turn (game-turn game)))
+      (select-max-child (lambda (node)
+                          (calc-ucb (uct-node-sum node)
+                                    (uct-node-num node)
+                                    (uct-node-num (get-node-value parent))
+                                    :coef  ucb-coef
+                                    :turn now-turn))
+                        parent))))
 
 (defun reflect-sim-result (game uct-tree uct-param result)
   (declare (ignore game uct-param))
@@ -112,15 +112,17 @@
                                 game
                                 (uct-node-move-from-parent (get-node-value child-tree))
                               (sim-once child-tree)))))))
-    (cadr (multiple-value-list (sim-once tree)))))
+    (multiple-value-bind (result tree) (sim-once tree)
+      (declare (ignore result))
+      tree)))
 
 (defun select-uct-node-by-ave (turn uct-tree)
-  (select-max-child #'(lambda (node)
-                        (let ((num (uct-node-num node))
-                              (sum (uct-node-sum node)))
-                          (if (> num 0)
-                              (* (/ sum num) (if (= turn +white+) 1 -1))
-                              -99999)))
+  (select-max-child (lambda (node)
+                      (let ((num (uct-node-num node))
+                            (sum (uct-node-sum node)))
+                        (if (> num 0)
+                            (* (/ sum num) (if (= turn +white+) 1 -1))
+                            -99999)))
                     uct-tree))
                     
 (defun uct-simulate (game param times)
